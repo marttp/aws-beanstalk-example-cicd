6dfb4341e943a5b162f393938abc8090726c4e90
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-06-10 15:30:18 +0700

    Initial project

---
+++ src/main/java/dev/tpcoder/AwsElasticBeanstalkCicdApplication.java
@@ -0,0 +1,13 @@
+package dev.tpcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstalkCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstalkCicdApplication.class, args);
+	}
+
+}
