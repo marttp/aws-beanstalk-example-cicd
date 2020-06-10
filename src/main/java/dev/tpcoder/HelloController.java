ab32651c22a1a0130c6c3084f7d3506110ca0cb2
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-06-10 16:45:04 +0700

    Correct Request Mapping

--- src/main/java/dev/tpcoder/HelloController.java
+++ src/main/java/dev/tpcoder/HelloController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hello")
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
